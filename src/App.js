0cb23c6ab9dc235ed4c701d761d71dc67c23f10f
Ana Carolina <kakarolina2006@hotmail.com>
2023-08-21 09:55:28 -0300

    tela de login e cadastro

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Cadastro from './paginas/cadastro/Cadastro';
+import Login from './paginas/login/Login';
+import CreateStyledGlobal from './styledGlobal';
 
 function App() {
   return (
-    <>
-    
-    </>
+   <>
+   <CreateStyledGlobal/>
+   <Cadastro/>
+   </>
   );
 }
 
